1299872ea86f6a9eea354ba561c6cbbabf3cac6d
elmorshedy-del <elmorshedy@icloud.com>
2026-08-18 12:59:00 -0400

    Expose Paper Trader as dedicated dashboard tab

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { LiveMonitor } from "@/components/LiveMonitor";
 import { MercuryDashboard } from "@/components/MercuryDashboard";
-import { PaperTradingControl } from "@/components/PaperTradingControl";
+import { PaperTraderTab } from "@/components/PaperTraderTab";
 import { getDashboard } from "@/lib/dashboard";
 
 export const dynamic = "force-dynamic";
@@ -9,8 +9,8 @@ export default async function Home() {
   const data = await getDashboard();
   return (
     <>
+      <PaperTraderTab />
       <MercuryDashboard initialData={data} />
-      <PaperTradingControl />
       <LiveMonitor />
     </>
   );
